9e30747551b14767911e6d5246021c4ef820875a
Naveen <150805268+Naveen-cloud-DevOps@users.noreply.github.com>
2025-06-28 16:00:13 +0530

    Update backend.tf

--- custom_network/backend.tf
+++ custom_network/backend.tf
@@ -1,8 +1,8 @@
 /*
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tfstate-rg"         # Resource group where the backend storage is created
-    storage_account_name  = "tfstateaccount123"  # Must be globally unique across Azure
+    resource_group_name   = "veera-rg"         # Resource group where the backend storage is created
+    storage_account_name  = "storageveera"  # Must be globally unique across Azure
     container_name        = "tfstate"            # Blob container for storing the state file
     key                   = "infra/terraform.tfstate"  # Path to the state file inside the container
   }
